8ed2247cca9079c4d4314d52ae5fea142e58df8c
김세미 <semikimm.m@gmail.com>
2022-04-11 21:52:29 +0900

    [spring_day1_hw] @Autowired 추가 & custom annotation 추가(@Exchange)

--- pairprogramming/src/main/java/com/nhnacademy/bank/domain/Bank.java
+++ pairprogramming/src/main/java/com/nhnacademy/bank/domain/Bank.java
@@ -1,11 +1,14 @@
 package com.nhnacademy.bank.domain;
 
+import com.nhnacademy.bank.annotation.Exchange;
 import com.nhnacademy.bank.exception.NegativeException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Bank {
     private final ExchangeService exchangeService;
 
-    public Bank(ExchangeService exchangeService) {
+    @Autowired
+    public Bank(@Exchange ExchangeService exchangeService) {
         this.exchangeService = exchangeService;
     }
 
